17391036d7b339606a76fa23f2b4e2289f597747
victor <victor.h@clarusway.com>
2023-10-21 23:19:48 +0200

    add posts with photo and poll

--- src/mock/posts.js
+++ src/mock/posts.js
@@ -1,6 +1,44 @@
 export const posts = [
 	{
 		id: '1708632465282150796',
+		type: 'poll',
+		content: `Do you like Javascript.`,
+		poll: {
+			voted: false,
+			votes: 12,
+			answers: [
+				{
+					id: 1,
+					text: 'Yes',
+					votes: 8
+				},
+				{
+					id: 2,
+					text: 'No',
+					votes: 1
+				},
+				{
+					id: 3,
+					text: 'Maybe',
+					votes: 3
+				}
+			]
+		},
+		account: {
+			avatar: 'https://pbs.twimg.com/profile_images/915569035743461376/QHMwMNG1_400x400.jpg',
+			username: 'devtnot_',
+			fullName: 'Devnot'
+		},
+		stats: {
+			comments: 43535,
+			repost: 3434,
+			like: 96946,
+			view: 903453
+		}
+	},
+	{
+		id: '1708632465282150796',
+		type: 'photo',
 		content: `i haven't done this in SOOO long
 
 		i'll be staying up way too late working through some very tedious bugs to make this library work
@@ -9,10 +47,7 @@ export const posts = [
 		
 		there is just something so fun about crushing outthings way into the night🤣`,
 		photos: [
-			'https://pbs.twimg.com/profile_images/1614986714795180033/yOQly3os_400x400.jpg',
-			'https://pbs.twimg.com/profile_images/1697749409851985920/HbrI04tM_400x400.jpg',
-			'https://pbs.twimg.com/media/F7VBIvaW0AAyXEr?format=jpg&name=large',
-			'https://pbs.twimg.com/media/F7VBIvaWAAAWUsx?format=jpg&name=large',
+			'https://pbs.twimg.com/media/F8xo4IAW0AA_k3J?format=jpg&name=4096x4096',
 		],
 		account: {
 			avatar: 'https://pbs.twimg.com/profile_images/1614986714795180033/yOQly3os_400x400.jpg',
@@ -28,9 +63,11 @@ export const posts = [
 	},
 	{
 		id: '1708632465282150796',
+		type: 'photo',
 		content: `Imagination Land!`,
 		photos: [
-			'https://pbs.twimg.com/media/F7ZIK8KWgAAP8pH?format=jpg&name=900x900'
+			'https://pbs.twimg.com/media/F7ZIK8KWgAAP8pH?format=jpg&name=900x900',
+			'https://pbs.twimg.com/media/F8vM2ctbcAErqOm?format=png&name=small'
 		],
 		account: {
 			avatar: 'https://pbs.twimg.com/profile_images/1683325380441128960/yRsRRjGO_400x400.jpg',
@@ -66,12 +103,12 @@ And people have been busy executing god-like tasks.
 	},
 	{
 		id: '1708810187170087410',
+		type: 'photo',
 		content: `Standing down from tonight’s Falcon 9 launch attempt due to upper level winds, teams are assessing the next available opportunity`,
 		photos: [
 			'https://pbs.twimg.com/profile_images/1697749409851985920/HbrI04tM_400x400.jpg',
 			'https://pbs.twimg.com/profile_images/1697749409851985920/HbrI04tM_400x400.jpg',
-			'https://pbs.twimg.com/profile_images/1697749409851985920/HbrI04tM_400x400.jpg',
-			'https://pbs.twimg.com/profile_images/1697749409851985920/HbrI04tM_400x400.jpg'
+			'https://pbs.twimg.com/media/F8zsUfbXoAAjPvZ?format=jpg&name=small'
 		],
 		account: {
 			avatar: 'https://pbs.twimg.com/profile_images/1697749409851985920/HbrI04tM_400x400.jpg',
@@ -87,12 +124,16 @@ And people have been busy executing god-like tasks.
 	},
 	{
 		id: '1708707876628476134',
+		type: 'photo',
 		content: `OpenAI's latest DALL-E 3 model is officially available free to try on Bing.
 The quality is insane, comparable to Midjourney.
 This prompt is going viral- mimicking a creepy iPhone capture that looks insanely realistic.
 Prompt in ALT tag + more examples in thread:`,
 		photos: [
-			'https://pbs.twimg.com/media/F7aKxHhaAAAnHlS?format=jpg&name=large'
+			'https://pbs.twimg.com/media/F8zqpUEWAAAuGFI?format=jpg&name=small',
+			'https://pbs.twimg.com/media/F8vJKAYa8AAhGFa?format=jpg&name=small',
+			'https://pbs.twimg.com/media/F8qwnY8a4AAYcpD?format=jpg&name=small',
+			'https://pbs.twimg.com/media/F8qv5S8bcAAxH8O?format=jpg&name=small',
 		],
 		account: {
 			avatar: 'https://pbs.twimg.com/profile_images/1711152452735774720/Cotttl-n_400x400.jpg',
